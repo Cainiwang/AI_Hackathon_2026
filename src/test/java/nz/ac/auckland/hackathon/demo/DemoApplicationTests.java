2bee54425a6e4bce459e7ecab496a9f49406d767
Caini Wang <cwan200@aucklanduni.ac.nz>
2026-07-11 18:17:47 +1200

    Initial commit with Maven and Springboot setup

---
+++ src/test/java/nz/ac/auckland/hackathon/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.hackathon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
